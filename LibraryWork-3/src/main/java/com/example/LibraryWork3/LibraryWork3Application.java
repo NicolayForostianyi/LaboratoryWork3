76db755b35d2a14eb128300d5bdf2b85d749fc01
Z3r0N <56758736+Z3r0N@users.noreply.github.com>
2022-11-08 13:21:12 +0500

    Add files via upload

---
+++ LibraryWork-3/src/main/java/com/example/LibraryWork3/LibraryWork3Application.java
@@ -0,0 +1,13 @@
+package com.example.LibraryWork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWork3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWork3Application.class, args);
+	}
+
+}
---
+++ LibraryWork-3/src/test/java/com/example/LibraryWork3/LibraryWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryWork3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWork3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
